05a1eb98753b131221b06529f218237b294d088c
Dylan Schmitz <dylanschmitz99@gmail.com>
2015-02-22 17:09:04 -0500

    added user profiles

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,7 +2,11 @@ class StaticPagesController < ApplicationController
 	def main_page
 		
 		if user_signed_in?
+			if current_user.profile
 			redirect_to new_tweet_path
+			else
+			redirect_to new_profile_path
+			end
 		end
 		@hometweetlink = "click here to tweet"
 	end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+
+
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -9,6 +11,8 @@ Rails.application.routes.draw do
     resources :tweets, except: [:edit, :show, :update, :delete]
 
     resources :relationships, only: :create 
+
+    resources :profiles, only: [:show, :new, :create]
 
     
 
