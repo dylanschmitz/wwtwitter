73f629273dcdf1e43f2c4676c37c01b731e8df59
Dylan Schmitz <dylanschmitz99@gmail.com>
2015-02-22 17:54:34 -0500

    changed production db to postgres, means in production use pg in developmetn we use sqlite3

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ActiveRecord::Base
 
   has_one :profile
 
-   has_many :active_relationships, class_name:  "Relationship",
+  has_many :active_relationships, class_name:  "Relationship",
                                   foreign_key: "follower_id",
                                   dependent:   :destroy
 
